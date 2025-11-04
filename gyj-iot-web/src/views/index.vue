5c102cf081dd914b25f0c63fe791439b51c598db
jishenghua <752718920@qq.com>
2025-11-04 23:30:18 +0800

    解决首页地图出不来的bug，注释了部分地图的配置

--- gyj-iot-web/src/views/index.vue
+++ gyj-iot-web/src/views/index.vue
@@ -432,120 +432,120 @@ function getmap() {
       zoom: 5,
       roam: true,
       mapStyle: {
-        styleJson: [
-          {
-            featureType: 'water',
-            elementType: 'all',
-            stylers: {
-              color: '#a0cfff',
-            },
-          },
-          {
-            featureType: 'land',
-            elementType: 'all',
-            stylers: {
-              color: '#fafafa', // #fffff8 淡黄色
-            },
-          },
-          {
-            featureType: 'railway',
-            elementType: 'all',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'highway',
-            elementType: 'all',
-            stylers: {
-              color: '#fdfdfd',
-            },
-          },
-          {
-            featureType: 'highway',
-            elementType: 'labels',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'arterial',
-            elementType: 'geometry',
-            stylers: {
-              color: '#fefefe',
-            },
-          },
-          {
-            featureType: 'arterial',
-            elementType: 'geometry.fill',
-            stylers: {
-              color: '#fefefe',
-            },
-          },
-          {
-            featureType: 'poi',
-            elementType: 'all',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'green',
-            elementType: 'all',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'subway',
-            elementType: 'all',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'manmade',
-            elementType: 'all',
-            stylers: {
-              color: '#d1d1d1',
-            },
-          },
-          {
-            featureType: 'local',
-            elementType: 'all',
-            stylers: {
-              color: '#d1d1d1',
-            },
-          },
-          {
-            featureType: 'arterial',
-            elementType: 'labels',
-            stylers: {
-              visibility: 'off',
-            },
-          },
-          {
-            featureType: 'boundary',
-            elementType: 'all',
-            stylers: {
-              color: '#999999',
-            },
-          },
-          {
-            featureType: 'building',
-            elementType: 'all',
-            stylers: {
-              color: '#d1d1d1',
-            },
-          },
-          {
-            featureType: 'label',
-            elementType: 'labels.text.fill',
-            stylers: {
-              color: '#999999',
-            },
-          },
-        ],
+        // styleJson: [
+        //   {
+        //     featureType: 'water',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#a0cfff',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'land',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#fafafa', // #fffff8 淡黄色
+        //     },
+        //   },
+        //   {
+        //     featureType: 'railway',
+        //     elementType: 'all',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'highway',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#fdfdfd',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'highway',
+        //     elementType: 'labels',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'arterial',
+        //     elementType: 'geometry',
+        //     stylers: {
+        //       color: '#fefefe',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'arterial',
+        //     elementType: 'geometry.fill',
+        //     stylers: {
+        //       color: '#fefefe',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'poi',
+        //     elementType: 'all',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'green',
+        //     elementType: 'all',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'subway',
+        //     elementType: 'all',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'manmade',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#d1d1d1',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'local',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#d1d1d1',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'arterial',
+        //     elementType: 'labels',
+        //     stylers: {
+        //       visibility: 'off',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'boundary',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#999999',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'building',
+        //     elementType: 'all',
+        //     stylers: {
+        //       color: '#d1d1d1',
+        //     },
+        //   },
+        //   {
+        //     featureType: 'label',
+        //     elementType: 'labels.text.fill',
+        //     stylers: {
+        //       color: '#999999',
+        //     },
+        //   },
+        // ],
       },
     },
     series: [
