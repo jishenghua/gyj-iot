32959ff4a3141dfec4cfccf72fd527aefc97f48e
jishenghua <752718920@qq.com>
2026-01-27 11:06:33 +0800

    优化产品分类接口

--- gyj-iot-boot/gyjiot-open-api/src/main/java/com/gyjiot/data/controller/CategoryController.java
+++ gyj-iot-boot/gyjiot-open-api/src/main/java/com/gyjiot/data/controller/CategoryController.java
@@ -1,36 +1,25 @@
 package com.gyjiot.data.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
-import com.gyjiot.common.core.domain.entity.SysRole;
-import com.gyjiot.common.core.domain.entity.SysUser;
-import com.gyjiot.common.core.page.TableDataInfo;
-import com.gyjiot.iot.model.IdAndName;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.gyjiot.common.annotation.Log;
 import com.gyjiot.common.core.controller.BaseController;
 import com.gyjiot.common.core.domain.AjaxResult;
+import com.gyjiot.common.core.page.TableDataInfo;
 import com.gyjiot.common.enums.BusinessType;
+import com.gyjiot.common.utils.poi.ExcelUtil;
 import com.gyjiot.iot.domain.Category;
 import com.gyjiot.iot.service.ICategoryService;
-import com.gyjiot.common.utils.poi.ExcelUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 产品分类Controller
- * 
+ *
  * @author jishenghua
  * @date 2021-12-16
  */
